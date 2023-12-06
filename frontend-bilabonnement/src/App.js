280591791851321fa279b74d39e80878d6628773
Jeppeneu123 <jeppeneumann@live.dk>
2023-12-06 13:59:48 +0100

    videre arbejde med routing

--- frontend-bilabonnement/src/App.js
+++ frontend-bilabonnement/src/App.js
@@ -3,15 +3,23 @@ import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import CarList from "./components/lists/carList";
 import SubscriptionList from "./components/lists/subscriptionList";
 import CustomerList from "./components/lists/customerList";
+import Navbar from "./components/websiteComponents/navbar";
 
 function App() {
   return (
       <Router>
           <div className="App">
-              <Link to="/cars" className="navbar-link"> <button  > Show Cars </button> </Link>
+              <header className="App-header">
+                  <Navbar />
+              </header>
+              <div className="content">
+              <Link to="/cars" className="navbar-link"> <button> Show Cars </button> </Link>
               <Routes>
                   <Route path="/cars" element={<CarList />} />
+                  <Route path="/customers" element={<CustomerList />} />
+                  <Route path="/subscriptions" element={<SubscriptionList />} />
               </Routes>
+              </div>
           </div>
       </Router>
 
