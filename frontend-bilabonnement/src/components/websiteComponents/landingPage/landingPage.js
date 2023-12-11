32bcafaedea40f820933b3d8d85f333bb861b537
Jeppeneu123 <jeppeneumann@live.dk>
2023-12-11 12:32:53 +0100

    redigering af navbar/landingpage

--- frontend-bilabonnement/src/components/websiteComponents/landingPage/landingPage.js
+++ frontend-bilabonnement/src/components/websiteComponents/landingPage/landingPage.js
@@ -5,16 +5,10 @@ import './landingPage.css';
 function LandingPage () {
     return (
         <div className="landing-page">
-            <h1>Bil abonnement develeoper app </h1>
+            <h1> Developer App </h1>
             <div className="features">
-                <Link className="cards" to="/cars">
-                    <span role="img" aria-label="Car Icon">🚗</span> Biler <div className="explanation"> <p> Oversigt over biler  </p> </div>
-                </Link>
-                <Link className="cards" to="/customers">
-                    <span role="img" aria-label="Person Icon">👤</span> Kunder <div className="explanation"> <p> Oversigt over kunder  </p> </div>
-                </Link>
-                <Link className="cards" to="/subscriptions">
-                    <span role="img" aria-label="Document Icon">📄</span> Abonnementer <div className="explanation"> <p> Oversigt over abonnementer  </p> </div>
+                <Link className="cards" to="/data">
+                    <span role="img" aria-label="Person Icon">👤</span> Data <div className="explanation"> <p> Oversigt over biler, kunder, abonnementer & skades reporter  </p> </div>
                 </Link>
                 <Link className="cards" to="/management">
                       <span role="img" aria-label="Settings Icon">⚙️</span> Management <div className="explanation"> <p> Her kan du lave nye biler, kunder og abonnementer </p> </div>
